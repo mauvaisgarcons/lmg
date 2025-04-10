b3b15ed3e6cf28e82b2d1366c3d9f9196457a8e9
Simon Bédard <simon@lesmauvaisgarcons.ca>
2025-04-10 14:04:28 -0400

    Small update to package

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     lib: {
         entry: 'vite-plugin/Js2Scss.js', // Entry point for your library (the SCSS plugin)
         name: 'Js2Scss',
-        fileName: (format) => `Js2Scss.${format}.js`, // Output file names
+        fileName: (format) => `Js2Scss.js`, // Output file names
     },
     outDir: 'dist', // Specify the output directory for your build
   },
